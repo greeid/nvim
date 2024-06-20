44914f761e8914cff328136af67771f4e24420d4
greeid <deekshit.bti@gmail.com>
2024-06-20 17:28:46 +0545

    `add` : add jdtls language server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "bashls", "pylsp", "lua_ls", "clangd", "marksman", "tsserver", "emmet_ls" }
+local servers = { "jdtls", "html", "cssls", "bashls", "pylsp", "lua_ls", "clangd", "marksman", "tsserver", "emmet_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
